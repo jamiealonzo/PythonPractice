bfae0e2b3912b24f50a3adbdd0042a0e30f09077
Jamie <79608519+jamiealonzo@users.noreply.github.com>
2023-05-05 10:15:16 -0700

    Create Dictionaries.py

---
+++ Dictionaries.py
@@ -0,0 +1,10 @@
+customer = {
+    "name": " John Wick",
+    "age": 45,
+    "is_verified": True
+}
+print(customer["name"])
+
+## can also add to the dictionary
+customer["birthday"] = "February 1 1978"
+print(customer["birthday"])
